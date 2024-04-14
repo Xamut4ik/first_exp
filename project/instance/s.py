fe829008fc6645844703cc90472a660ae9494e61
Xamut4ik <152077459+Xamut4ik@users.noreply.github.com>
2024-04-14 14:24:57 +0300

    Delete project directory

--- project/instance/s.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-def delete_messages():
-    conn = sqlite3.connect("news.db")
-    c = conn.cursor()
-    c.execute('DELETE FROM article WHERE id = 1')
-    conn.commit()
-    conn.close()
-
-delete_messages()
